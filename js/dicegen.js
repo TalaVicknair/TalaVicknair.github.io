c6d07f61d6604ec913cf79a8608971f420516c0b
TalaVicknair <Tala.Vicknair@Colorado.EDU>
2022-12-01 15:15:17 -0700

    final

---
+++ js/dicegen.js
@@ -0,0 +1,26 @@
+// window.addEventListener('load', randomAll);
+
+const baseColorbtn = document.querySelector('bc').addEventListener("click", writeBaseClr);
+
+const baseColor = ["Clear","Red","Orange","Yellow","Green","Blue","Purple","Pink","Gold","Silver","Black","White"];
+const extraColor = ["None","Red","Orange","Yellow","Green","Blue","Purple","Pink","Gold","Silver","Black","White"];
+const glitter = ["Glitter","Pearlescent","Translucent","Opaque"];
+const shine = ["Matte","Shiny"];
+const inclusions = ["Fruit","Cards","Glitter","Holographic Paper","Flowers"];
+const technique = ["Petri","Swirl","Marble","Clouds"];
+const inkColor = ["Red","Orange","Yellow","Green","Blue","Purple","Pink","Gold","Silver","Black","White"];
+const d4Shape = ["Pyramid","Pendant","Gem"];
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+};
+
+function writeBaseClr() {
+    displaybaseColor(baseColor(getRandomIntInclusive(1, 12)));
+};
+
+function displaybaseColor(x) {
+    document.getElementById("baseColortxt").textContent = x;
+};
